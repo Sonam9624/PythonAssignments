b47ec88139160cc984c76c8f279afa835087700a
Sonam9624 <37657997+Sonam9624@users.noreply.github.com>
2025-05-06 14:47:08 +0530

    Add files via upload

--- Bike.py
+++ Bike.py
@@ -1,2 +1,13 @@
 
 price=int(input("Enter price of bike: "))
+tax=0
+
+if price>80000:
+    tax=(15*price)/100
+    print("15% tax is:", tax)
+elif (price > 40000 and price<80000): 
+       tax=(10*price)/100
+       print("10% tax is:", tax)
+else:  
+       tax=(5*price)/100
+       print("5% tax is:", tax)
